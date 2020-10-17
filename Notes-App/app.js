debf190a7e1acaa92d5543c10269536a726abe9c
Blake Fryman <blakefryman12@gmail.com>
2020-10-17 11:27:58 -0500

    Updated Notes-App

---
+++ Notes-App/app.js
@@ -0,0 +1,35 @@
+const validator = require('validator')
+const getNotes = require('./notes')
+
+const msg = getNotes()
+console.log(msg)
+
+console.log(validator.isEmail('blake@example.com'))
+console.log(validator.isEmail('example.com'))
+
+console.log(validator.isURL('https://google.com'))
+console.log(validator.isURL('https:/website'))
+
+
+
+
+// const add = require('./utils')
+
+// const sum = add(4, -2)
+
+// console.log(sum)
+
+/*
+ Challege: Define and use a function in a new file
+
+ 1. Create a new file called notes.js
+ 2. Create getNotes function that returns "Your notes..."
+ 3. Export getNotes function
+ 4. From app.js, load in and call the function printing message to console
+*/
+
+// const getNotes = require('./notes')
+
+// const msg = getNotes()
+
+// console.log(msg)
