32cae62cfca7bfc93017e1a9beb92d41ab626160
Blake Fryman <blakefryman12@gmail.com>
2020-10-19 10:34:09 -0500

    Updated

--- Weather-App/app.js
+++ Weather-App/app.js
@@ -1,18 +1,29 @@
-const request = require('request')
 const chalk = require('chalk')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-forecast(42.3605, -71.0596, (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
+const address = process.argv[2]
 
-
-
+if (!address) {
+  console.log(chalk.red.bold('Please provide an address.'))
+} else {
 // Geocoding (Forward Geocoding)
 // Address -> Lat/Long -> Weather
-geocode('Boston', (error, data) => {
-  console.log(chalk.red.bold('Error', error))
-  console.log('Data', data)
+geocode(address, (error, data) => {
+  if (error) {
+    return console.log(chalk.red.bold(error))
+  }
+
+  // Geocoding (Forward Geocoding)
+  // Address -> Lat/Long -> Weather
+  forecast(data.lat, data.long, (error, forecastData) => {
+    if (error) {
+      return console.log(chalk.red.bold(error))
+    }
+
+    console.log(data.location)
+    console.log(forecastData)
+  })
 })
+}
+
