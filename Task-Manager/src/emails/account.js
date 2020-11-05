cf1fc0e3c6c1c03c3ab19ece3902c43239fe02a1
Blake Fryman <blakefryman12@gmail.com>
2020-11-05 11:01:33 -0600

    Update

--- Task-Manager/src/emails/account.js
+++ Task-Manager/src/emails/account.js
@@ -7,7 +7,7 @@ sgMail.setApiKey(sendgridAPIKey);
 const sendWelcomeEmail = (email, name) => {
   sgMail.send({
     to: email,
-    from: 'blakefryman12@gmail.com',
+    from: '*',
     subject: 'Welcome to Task Manager!',
     text: `Welcome to the app, ${name}! Let me know how you get along with the app.`
   })
@@ -16,7 +16,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancelationEmail = (email, name) => {
   sgMail.send({
     to: email,
-    from: 'blakefryman12@gmail.com',
+    from: '*',
     subject: 'Hate To See You Go!',
     text: `Thank you for using the Task Manager app, ${name}!`
   })
